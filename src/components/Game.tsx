3c770c85165d2a4684695bf7c54976ee219cd3d1
Mika Doorenbosch <mika.doorenbosch@gmail.com>
2022-12-18 14:49:35 +0100

    implementation undo button

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,27 +4,27 @@ import getIpaSymbols from "../ipaUtils/getIpaSymbols";
 import SymbolTypeButtons from "./SymbolTypeButtons";
 import VowelButtons from "./VowelButtons";
 import ConsonantButtons from "./ConsonantButtons";
-import playAudio from "./playAudio"
-import checkAnswer from "./checkAnswer"
-import getPhoneticsPreset from "../data/presets/phonetics"
+import playAudio from "./playAudio";
+import checkAnswer from "./checkAnswer";
+import getPhoneticsPreset from "../data/presets/phonetics";
 import "./Game.css";
 
 function setupGame() {
-  // const symbols: Array<ipaEntry> = getIpaSymbols();
   const [symbols, setSymbols] = useState(getIpaSymbols(getPhoneticsPreset()));
-  const [choices, setChoices] = useState(generateChoices(symbols))
+  const [choices, setChoices] = useState(generateChoices(symbols));
   const nextSymbol = () => {
     const randomIndex: number = Math.floor(Math.random() * symbols.length);
     return symbols[randomIndex];
   };
   const setIpaSet = (ipaSet?: Array<number>) => {
     setSymbols(getIpaSymbols(ipaSet));
-    setChoices(generateChoices(symbols))
+    setChoices(generateChoices(symbols));
   };
   return { nextSymbol, choices, setIpaSet };
 }
 
 function Game() {
+  const inputHistory: string[] = [];
   const { nextSymbol, choices, setIpaSet } = setupGame();
   const [ipaSymbol, setIpaSymbol] = useState(nextSymbol());
   const [answer, setAnswer] = useState("");
@@ -37,13 +37,31 @@ function Game() {
   };
   const updateAnswer = (choice: string) => {
     setAnswer((oldAnswer) => {
+      inputHistory.push(choice);
       return oldAnswer + " " + choice;
     });
   };
 
   const undo = () => {
-    console.log("undo")
-  }
+    inputHistory.pop();
+    if (inputHistory.length > 0) {
+      let newAnswer = "";
+      inputHistory.forEach((input) => {
+        newAnswer = newAnswer + " " + input;
+      });
+      setPage((oldPage) => (oldPage > 0 ? oldPage - 1 : 0));
+      setAnswer(newAnswer);
+    } else {
+      setPage((oldPage) => (oldPage > 0 ? oldPage - 1 : 0));
+      setAnswer("");
+    }
+  };
+
+  const clearHistory = () => {
+    while (inputHistory.length > 0) {
+      inputHistory.pop();
+    }
+  };
   const showAnswer = (): string => {
     return `${ipaSymbol.articulation.firstDimension} ${
       ipaSymbol.articulation.secondDimension
@@ -54,7 +72,7 @@ function Game() {
   useEffect(() => {
     if (page === 4) {
       const answerString = `${ipaSymbol.articulation.firstDimension} ${ipaSymbol.articulation.secondDimension} ${ipaSymbol.articulation.thirdDimension}`;
-      const result = checkAnswer(answerString, answer.trim())
+      const result = checkAnswer(answerString, answer.trim());
       if (result) {
         setIsCorrect("correct");
       } else {
@@ -66,7 +84,9 @@ function Game() {
   return (
     <div className="game-container">
       <div className="prompt-container">
-        <div className="ipa-prompt ipa" onClick={() => playAudio(ipaSymbol)}>{ipaSymbol.symbol}</div>
+        <div className="ipa-prompt ipa" onClick={() => playAudio(ipaSymbol)}>
+          {ipaSymbol.symbol}
+        </div>
       </div>
       <div className="preset-container">
         <button
@@ -95,10 +115,14 @@ function Game() {
         </button>
       </div>
       <div className="answer-container">
-        {page === 4?<div className="correct-answer-prompt">{showAnswer()}</div>:undefined}
+        {page === 4 ? (
+          <div className="correct-answer-prompt">{showAnswer()}</div>
+        ) : undefined}
         <div className="answer-input-container">
-        <div className={`answer-prompt ${isCorrect}`}>{answer}</div>
-        <button className="undo-button btn" onClick={undo}>undo</button>
+          <div className={`answer-prompt ${isCorrect}`}>{answer}</div>
+          <button className="undo-button btn" onClick={undo}>
+            undo
+          </button>
         </div>
       </div>
       <div className="button-container">
@@ -126,6 +150,7 @@ function Game() {
             setPage(0);
             setIpaSymbol(nextSymbol());
             setIsCorrect("pending");
+            clearHistory();
           }}
           className="next-symbol-btn btn"
         >
